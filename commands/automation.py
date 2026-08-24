c46e6a62dbd77b162aa65affe2b70a82d3e476cb
fdsaf23 <cuvak2077@gmail.com>
2026-08-24 14:53:25 +0700

    Add files via upload

---
+++ commands/automation.py
@@ -0,0 +1,33 @@
+from pyrogram import filters, enums
+from core import app
+from cfg import PREFIXES
+import time
+
+# .afk — включить AFK
+# .unafk — выключить AFK
+# .autoreply — автоответчик
+# .delreply — удалить автоответ
+# .replies — список автоответов
+# .autoreact — авто-реакции
+
+AFK = False
+AFK_REASON = "👋 Привет, я автоответчик! \nЖди ответа в течении 2-5 часов если я не в сети, если ответа нет - пиши в @feedbackZLX_bot \nОт спама в лс смысла нет ведь уведомления выключены"
+
+@app.on_message(filters.me & filters.command("afk", prefixes=PREFIXES))
+async def afk(client, message):
+
+    global AFK, AFK_REASON
+
+    AFK = True
+
+    await message.edit("💤 АФК включен")
+
+@app.on_message(~filters.me & filters.private & filters.incoming)
+async def afk_reply(client, message):
+
+    global AFK
+
+    if not AFK:
+        return
+
+    await message.reply(AFK_REASON)
