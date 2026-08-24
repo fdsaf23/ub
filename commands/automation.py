c73e9ada4425f69ac8333c696532016eef35fc46
fdsaf23 <cuvak2077@gmail.com>
2026-08-24 15:28:31 +0700

    Update automation.py

--- commands/automation.py
+++ commands/automation.py
@@ -11,7 +11,8 @@ import time
 # .autoreact — авто-реакции
 
 AFK = False
-AFK_REASON = "<b>👋 Привет, я автоответчик! \n-------------------\n<u>Жди ответа в течении 2-5 часов если я не в сети, если ответа нет - пиши в @feedbackZLX_bot</u> \n-------------------\nОт спама в лс смысла нет ведь уведомления выключены</b>"
+AFK_REPLY_SLEEP = 0
+AFK_REASON = "<b>👋 Привет, я автоответчик! \n-------------------\n<i>Жди ответа в течении 2-5 часов если я не в сети, если ответа нет - пиши в @feedbackZLX_bot</i> \n-------------------\nОт спама в лс смысла нет ведь уведомления выключены</b>"
 
 @app.on_message(filters.me & filters.command("afk", prefixes=PREFIXES))
 async def afk(client, message):
@@ -28,7 +29,7 @@ async def afk(client, message):
 @app.on_message(~filters.me & filters.private & filters.incoming)
 async def afk_reply(client, message):
 
-    global AFK
+    global AFK, AFK_REPLY_SLEEP
 
     if not AFK:
         return
@@ -36,9 +37,12 @@ async def afk_reply(client, message):
     if message.from_user and message.from_user.is_bot:
         return
 
-    if AFK:
-        await message.reply(AFK_REASON, parse_mode = enums.ParseMode.HTML)
-        time.sleep(15)
+    now = time.time()
+
+    if now - AFK_REPLY_SLEEP < 15:
+        return
+
+    await message.reply(AFK_REASON, parse_mode = enums.ParseMode.HTML)
 
 @app.on_message(filters.me & filters.command("unafk", prefixes = PREFIXES))
 async def unafk(client, message):
