6ffbeba3677f865c2b0ea93df0e867cf6a278115
fdsaf23 <cuvak2077@gmail.com>
2026-08-24 19:48:38 +0700

    Update fun.py

--- commands/fun.py
+++ commands/fun.py
@@ -9,3 +9,18 @@ from pyrogram import filters, enums
 from core import app
 from cfg import PREFIXES
 
+font = "fonts/FredokaOneCyrillic-Regular.ttf"
+
+@app.on_message(filters.me & filters.command("quote", prefixes = PREFIXES))
+async def quote(client, message):
+
+  reply = message.reply_to_message
+
+  if not reply and not reply.from_user:
+    return await message.edit("❌ Используй реплеем")
+
+  user = reply.from_user
+  text = textwrap.fill(target.text, width = 50)
+
+  if user.photo:
+    avatar = await client.download_media()
