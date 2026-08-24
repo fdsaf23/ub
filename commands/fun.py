90dd0542b16e88bc41924a6ad298c0c594105187
fdsaf23 <cuvak2077@gmail.com>
2026-08-25 00:06:13 +0700

    Update fun.py

--- commands/fun.py
+++ commands/fun.py
@@ -256,27 +256,7 @@ async def typing(client, message):
 
     args = message.text.split(maxsplit=2)
 
-    if len(args) == 1:
-
-        if chat_id in typing_active:
-            return await message.edit("❌ Тайпинг уже запущен, для остановки: <code>.typing stop</code>")
-    
-        chat = await client.get_chat(chat_id)
-        chat_name = chat.title or chat.first_name
-    
-        await message.edit(f"⌨️ Тайпинг в чате <b>{chat_name}</b> запущен")
-    
-        async def typing_loop():
-            try:
-                while chat_id in typing_active:
-                    await app.send_chat_action(chat_id, enums.ChatAction.TYPING)
-                    await asyncio.sleep(8)
-            finally:
-                typing_active.pop(chat_id, None)
-    
-        typing_active[chat_id] = asyncio.create_task(typing_loop())
-
-    if args[1].lower() == "stop":
+    if len(args) > 1 and args[1].lower() == "stop":
 
         if chat_id not in typing_active:
             return await message.edit("❌ Тайпинг не запущен")
@@ -287,6 +267,24 @@ async def typing(client, message):
         del typing_active[chat_id]
 
         await message.edit(f"✅ Тайпинг в чате <b>{chat_name}</b> остановлен")
+
+    if chat_id in typing_active:
+        return await message.edit("❌ Тайпинг уже запущен, для остановки: <code>.typing stop</code>")
+    
+    chat = await client.get_chat(chat_id)
+    chat_name = chat.title or chat.first_name
+    
+    await message.edit(f"⌨️ Тайпинг в чате <b>{chat_name}</b> запущен")
+    
+    async def typing_loop():
+        try:
+            while chat_id in typing_active:
+                await app.send_chat_action(chat_id, enums.ChatAction.TYPING)
+                await asyncio.sleep(8)
+        finally:
+            typing_active.pop(chat_id, None)
+    
+    typing_active[chat_id] = asyncio.create_task(typing_loop())
 
 
 
