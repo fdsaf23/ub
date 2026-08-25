d75672ce7d3e24c754b12746ea729beeda18fcf6
fdsaf23 <cuvak2077@gmail.com>
2026-08-25 19:51:48 +0700

    Update fun.py

--- commands/fun.py
+++ commands/fun.py
@@ -352,17 +352,13 @@ async def bull_loop(client, message):
 
 @app.on_message(filters.me & filters.command('type', prefixes = PREFIXES))
 async def type_anim(client, message):
-    args = message.text.split(maxsplit=2)
+    args = message.text.split(maxsplit=3)
     orig_text = args[2]
     text = orig_text
     tbp = ""
-    symbol = args[1]
+    symbol = "|"
 
-    if len(args) == 3:
-        symbol = "|"
-        return
-
-    if len(args) <= 1:
+    if len(args) == 1:
         return await message.edit("❌ Используй: .type symbol text")
 
     while(tbp != orig_text):
