6c2c9c00127ccd8f13d56bd75ee66c56f6637b96
fdsaf23 <cuvak2077@gmail.com>
2026-08-25 22:21:00 +0700

    Update fun.py

--- commands/fun.py
+++ commands/fun.py
@@ -406,7 +406,16 @@ async def dice_send(client, message):
         return
 
     player1 = message.dice.value
-    player2 = await client.send_dice(chat_id = message.chat.id, emoji = "🎲")
+    player2 = await client.send_dice(chat_id = message.chat.id, reply_to_message_id=message.id, emoji = "🎲")
+
+    await asyncio.sleep(3.5)
+
+    if player1 > player2.dice.value:
+        return await message.reply(f"Удача на твоей стороне. {player1} VS {player2.dice.value}")
+    elif player1 < player2.dice.value:
+        return await message.reply(f"Моя взяла. {player2.dice.value} VS {player1}")
+    else:
+        return await message.reply("Судьба не выбрала победителя.")
     
 
 
