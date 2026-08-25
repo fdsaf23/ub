7e1fba62fa41d2c30463e77d7425cab24afc37c7
fdsaf23 <cuvak2077@gmail.com>
2026-08-26 01:52:43 +0700

    Update msg_user.py

--- commands/msg_user.py
+++ commands/msg_user.py
@@ -1,10 +1,13 @@
 from pyrogram import filters, enums
-from pyrogram.errors import FloodWait
+from pyrogram.raw.types import UpdateDeleteMessages, UpdateDeleteChannelMessages
 import asyncio
 from core import app
 from cfg import PREFIXES
+import os
 
 muted_users = set()
+group_save_id = -5596420347
+msg_cache = {}
 
 @app.on_message(filters.me & filters.private & filters.command("mute", prefixes = PREFIXES))
 async def mute(client, message):
@@ -50,6 +53,85 @@ async def del_msg(client, message):
   except Exception as e:
     await message.reply(e)
 
+@app.on_message(filters.private & filters.incoming, group = 4)
+async def save_msg(client, message):
 
+  chat_id = message.chat.id
+
+  if chat_id == group_save_id:
+    return
+
+  if message.from_user == message.from_user.is_self:
+    return
+
+  key = f"{chat_id}:{message.id}"
+
+  data = {
+    "chat_id": chat_id,
+    "message_id": message.id,
+    "text": message.text or message.caption or "",
+    "user_id": message.from_user.id,
+    "name": message.from_user.first_name,
+    "media": None
+  }
+  
+  if message.media:
+    try:
+      os.makedirs("archive_media", exits_ok=True)
+
+      file_path = await message.download(f"archive_media/{chat_id}_{message.id}")
+
+      data["media"] = data
+      
+    except Exception as e:
+      print(e)
+
+  msg_cache[key] = data
+
+@app.on_raw_updates()
+async def delected_msg(client, update, users, chats):
+  if isinstance(update, UpdateDeleteMessage):
+    return
+
+  delete_ids = update.messages
+
+  for key, data in list(msg_cache.items()):
+
+    if data["message_id"] not in delete_ids:
+      continue
+
+    name = data['name']
+    user_id = data['user_id']
+    text = data['text']
+
+    caption = f"""
+🗑 Сообщение было удалено от <b><a href='tg://user?id={user_id}'>{name}</a></b>
+
+📋 Содержимое:
+<b><i>{text}</i></b>
+"""
+
+  try:
+    if (data['media'] and os.path.exists(data['media'])):
+      return await client.send_message(chat_id=group_save_id, data['media'], caption=caption)
+    else:
+      await client.send_message(chat_id=group_save_id, caption)
+
+  except Exception as e:
+    print(e)
+
+  del msg_cache[key]
+
+  
+
+
+
+
+
+
+
+
+
+  
 
 
