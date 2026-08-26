f7f4c6ba2b129e7db123183d868456281b199493
fdsaf23 <cuvak2077@gmail.com>
2026-08-26 20:57:11 +0700

    Update msg_user.py

--- commands/msg_user.py
+++ commands/msg_user.py
@@ -57,7 +57,7 @@ async def interaction_cmd(client, message):
   text = message.command[0].lower()
 
   if text.lower() == "hug":
-    await client.send_message(chat_id=message.chat_id, "ты обнял его", reply_to_message_id=message.id)
+    await client.send_message(chat_id=message.chat_id, text="ты обнял его", reply_to_message_id=message.id)
 
 
 
