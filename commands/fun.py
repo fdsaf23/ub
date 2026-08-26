ae93539e55f60377e890704dc7057d815972f87f
fdsaf23 <cuvak2077@gmail.com>
2026-08-27 03:28:30 +0700

    Update fun.py

--- commands/fun.py
+++ commands/fun.py
@@ -202,7 +202,7 @@ async def wanted_user(client, message):
     bg.alpha_composite(avatar_img, (avatar_x, 150))
     
     draw.text((200, 45), "WANTED", font=font_wanted, fill=(252, 0, 50), anchor="mm")
-    draw.text((200, 200), f"- {reply.first_name} -", font=font_wanted_user, fill=(199, 199, 199), anchor="mm")
+    draw.text((200, 200), f"- {user.first_name} -", font=font_wanted_user, fill=(199, 199, 199), anchor="mm")
 
     final_buffer = BytesIO()
     bg.convert("RGB").save(final_buffer, "JPEG", qualite = 85)
