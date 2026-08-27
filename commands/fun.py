c99878b9191af2d34ebb5155e1d5953593306642
fdsaf23 <cuvak2077@gmail.com>
2026-08-27 21:32:37 +0700

    Update fun.py

--- commands/fun.py
+++ commands/fun.py
@@ -72,10 +72,10 @@ async def music_card(client, message):
     draw.text((text_x, 90), title, font=font_music_title, fill="white")
     draw.text((text_x, 150), author, font=font_music_artist, fill=(167, 167, 167))
 
-    bar_x, bar_y, bar_w, bar_h = text_x, 280, 560, 7
-    draw.rounded_rectangle((bar_x, bar_y, bar_x + bar_w, bar_y + bar_h), radius = 8, fill = (15, 2, 166))
-    draw.text((bar_x, bar_y + 20), "0:00", fill = (167, 167, 167))
-    draw.text((bar_x + bar_w, bar_y + 20), duration_str, fill = (167, 167, 167))
+    bar_x, bar_y, bar_w, bar_h = text_x, 280, 560, 9
+    draw.rounded_rectangle((bar_x, bar_y, bar_x + bar_w, bar_y + bar_h), radius = 8, fill = (113, 104, 217))
+    draw.text((bar_x, bar_y + 20), "0:00", font = font_music_time, fill = (167, 167, 167))
+    draw.text((bar_x + bar_w - 30, bar_y + 20), duration_str, font = font_music_time, fill = (167, 167, 167))
     
     
     final_buffer = BytesIO()
