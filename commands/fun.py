d8a613a81efaa7805b3830766b85f7622b781cfc
fdsaf23 <cuvak2077@gmail.com>
2026-08-27 20:14:40 +0700

    Update fun.py

--- commands/fun.py
+++ commands/fun.py
@@ -52,7 +52,7 @@ async def music_card(client, message):
 
     bg = raw_img.resize((1000, 400))
     bg = bg.filter(ImageFilter.BoxBlur(radius = 17))
-    overlay = Image.new("RGBA", bg.size, fill = (0, 0, 0, 150))
+    overlay = Image.new("RGBA", bg.size, (0, 0, 0, 160))
     bg = Image.alpha_composite(bg.convert("RGB"), overlay)
 
     draw = ImageDraw.draw(bg)
